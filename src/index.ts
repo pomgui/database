2dcac0d73423cf3e3bf95aaafd99d78d327ea002
wpomier <55899540+pomgui@users.noreply.github.com>
2020-09-06 23:40:00 -0300

    Added mysqlpool to exported list

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from './pi-no-database';
 export * from './firebird/pi-firebird-database';
 export * from './firebird/pi-firebird-pool';
 export * from './mysql/pi-mysql-database';
+export * from './mysql/pi-mysql-pool';
 export * from './tools';
